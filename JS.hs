15c6742b353f78e364bd85ef5b6e1cc3fa63858d
Lykd <lykd@pm.me>
2025-10-22 15:37:26 +0300

    It all works, kinda.

--- JS.hs
+++ JS.hs
@@ -33,6 +33,7 @@ import Control.Arrow ((>>>))
 import Control.Monad (replicateM_, void, when, (>=>))
 import GHC.JS.Prim (JSVal, fromJSInt, toJSArray, toJSInt, toJSString)
 import Relude
+import System.IO.Unsafe (unsafePerformIO)
 
 foreign import javascript "(()=>window)" window :: IO JSVal
 
@@ -42,162 +43,45 @@ foreign import javascript "((el, sel, val) => {el[sel] = val })" set_ :: JSVal -
 
 foreign import javascript "((obj) => {new obj()})" new :: JSVal -> IO JSVal
 
+foreign import javascript "((f, list) => f(...list))" callList_ :: JSVal -> JSVal -> IO JSVal
+
+foreign import javascript "(f => f())" call0 :: JSVal -> IO JSVal
+
 (.>) :: (ToJSVal a) => JSVal -> a -> IO JSVal
-a .> b = toJSVal b >>= attr a
+a .> b = attr a (toJSVal b)
 
 (.>>) :: (ToJSVal b) => IO JSVal -> b -> IO JSVal
 a .>> b = a >>= (.> b)
 
-llac :: (ToJSVal a) => (ToJSVal b) => a -> b -> IO JSVal
-llac = flip call1
-
 set :: (ToJSVal a) => (ToJSVal b) => JSVal -> b -> a -> IO ()
-set obj key val = do
-  k <- toJSVal key
-  v <- toJSVal val
-  void $ set_ obj k v
+set obj key val = void $ set_ obj (toJSVal key) (toJSVal val)
 
 class ToJSVal a where
-  toJSVal :: a -> IO JSVal
+  toJSVal :: a -> JSVal
 
 instance ToJSVal Int where
-  toJSVal = pure . toJSString . show
+  toJSVal = toJSString . show
 
 instance ToJSVal Double where
-  toJSVal = pure . toJSString . show
+  toJSVal = toJSString . show
 
 instance ToJSVal String where
-  toJSVal = pure . toJSString
+  toJSVal = toJSString
 
 instance ToJSVal JSVal where
-  toJSVal = pure
+  toJSVal = id
 
 instance ToJSVal [JSVal] where
-  toJSVal = toJSArray
-
-foreign import javascript "(el => el())" call0 :: JSVal -> IO JSVal
-
-foreign import javascript "((el, a1) => el(a1))" call1_ :: JSVal -> JSVal -> IO JSVal
-
-foreign import javascript "((el, a1, a2) => el(a1, a2))" call2_ :: JSVal -> JSVal -> JSVal -> IO JSVal
-
-foreign import javascript "((el, a1, a2, a3) => el(a1, a2, a3))" call3_ :: JSVal -> JSVal -> JSVal -> JSVal -> IO JSVal
-
-foreign import javascript "((el, a1, a2, a3, a4) => el(a1, a2, a3, a4))" call4_ :: JSVal -> JSVal -> JSVal -> JSVal -> JSVal -> IO JSVal
-
-foreign import javascript "((el, a1, a2, a3, a4, a5) => el(a1, a2, a3, a4, a5))" call5_ :: JSVal -> JSVal -> JSVal -> JSVal -> JSVal -> JSVal -> IO JSVal
-
-foreign import javascript "((el, a1, a2, a3, a4, a5, a6) => el(a1, a2, a3, a4, a5, a6))" call6_ :: JSVal -> JSVal -> JSVal -> JSVal -> JSVal -> JSVal -> JSVal -> IO JSVal
-
-foreign import javascript "((el, a1, a2, a3, a4, a5, a6, a7) => el(a1, a2, a3, a4, a5, a6, a7))" call7_ :: JSVal -> JSVal -> JSVal -> JSVal -> JSVal -> JSVal -> JSVal -> JSVal -> IO JSVal
-
-foreign import javascript "((el, a1, a2, a3, a4, a5, a6, a7, a8) => el(a1, a2, a3, a4, a5, a6, a7, a8))" call8_ :: JSVal -> JSVal -> JSVal -> JSVal -> JSVal -> JSVal -> JSVal -> JSVal -> JSVal -> IO JSVal
-
-foreign import javascript "((el, a1, a2, a3, a4, a5, a6, a7, a8, a9) => el(a1, a2, a3, a4, a5, a6, a7, a8, a9))" call9_ :: JSVal -> JSVal -> JSVal -> JSVal -> JSVal -> JSVal -> JSVal -> JSVal -> JSVal -> JSVal -> IO JSVal
-
-foreign import javascript "((f, list) => f(...list))" callList_ :: JSVal -> JSVal -> IO JSVal
-
-callList :: JSVal -> [JSVal] -> IO JSVal
-callList f list = toJSArray list >>= callList_ f
-
-call1 :: (ToJSVal a, ToJSVal b) => a -> b -> IO JSVal
-call1 func arg1 = do
-  func' <- toJSVal func
-  arg1' <- toJSVal arg1
-  call1_ func' arg1'
-
-call2 :: (ToJSVal a, ToJSVal b, ToJSVal c) => a -> b -> c -> IO JSVal
-call2 func arg1 arg2 = do
-  func' <- toJSVal func
-  arg1' <- toJSVal arg1
-  arg2' <- toJSVal arg2
-  call2_ func' arg1' arg2'
-
-call3 :: (ToJSVal a, ToJSVal b, ToJSVal c, ToJSVal d) => a -> b -> c -> d -> IO JSVal
-call3 func arg1 arg2 arg3 = do
-  func' <- toJSVal func
-  arg1' <- toJSVal arg1
-  arg2' <- toJSVal arg2
-  arg3' <- toJSVal arg3
-  call3_ func' arg1' arg2' arg3'
-
-call4 :: (ToJSVal a, ToJSVal b, ToJSVal c, ToJSVal d, ToJSVal e) => a -> b -> c -> d -> e -> IO JSVal
-call4 func arg1 arg2 arg3 arg4 = do
-  func' <- toJSVal func
-  arg1' <- toJSVal arg1
-  arg2' <- toJSVal arg2
-  arg3' <- toJSVal arg3
-  arg4' <- toJSVal arg4
-  call4_ func' arg1' arg2' arg3' arg4'
-
-call5 :: (ToJSVal a, ToJSVal b, ToJSVal c, ToJSVal d, ToJSVal e, ToJSVal f) => a -> b -> c -> d -> e -> f -> IO JSVal
-call5 func arg1 arg2 arg3 arg4 arg5 = do
-  func' <- toJSVal func
-  arg1' <- toJSVal arg1
-  arg2' <- toJSVal arg2
-  arg3' <- toJSVal arg3
-  arg4' <- toJSVal arg4
-  arg5' <- toJSVal arg5
-  call5_ func' arg1' arg2' arg3' arg4' arg5'
-
-call6 :: (ToJSVal a, ToJSVal b, ToJSVal c, ToJSVal d, ToJSVal e, ToJSVal f, ToJSVal g) => a -> b -> c -> d -> e -> f -> g -> IO JSVal
-call6 func arg1 arg2 arg3 arg4 arg5 arg6 = do
-  func' <- toJSVal func
-  arg1' <- toJSVal arg1
-  arg2' <- toJSVal arg2
-  arg3' <- toJSVal arg3
-  arg4' <- toJSVal arg4
-  arg5' <- toJSVal arg5
-  arg6' <- toJSVal arg6
-  call6_ func' arg1' arg2' arg3' arg4' arg5' arg6'
-
-call7 :: (ToJSVal a, ToJSVal b, ToJSVal c, ToJSVal d, ToJSVal e, ToJSVal f, ToJSVal g, ToJSVal h) => a -> b -> c -> d -> e -> f -> g -> h -> IO JSVal
-call7 func arg1 arg2 arg3 arg4 arg5 arg6 arg7 = do
-  func' <- toJSVal func
-  arg1' <- toJSVal arg1
-  arg2' <- toJSVal arg2
-  arg3' <- toJSVal arg3
-  arg4' <- toJSVal arg4
-  arg5' <- toJSVal arg5
-  arg6' <- toJSVal arg6
-  arg7' <- toJSVal arg7
-  call7_ func' arg1' arg2' arg3' arg4' arg5' arg6' arg7'
-
-call8 :: (ToJSVal a, ToJSVal b, ToJSVal c, ToJSVal d, ToJSVal e, ToJSVal f, ToJSVal g, ToJSVal h, ToJSVal i) => a -> b -> c -> d -> e -> f -> g -> h -> i -> IO JSVal
-call8 func arg1 arg2 arg3 arg4 arg5 arg6 arg7 arg8 = do
-  func' <- toJSVal func
-  arg1' <- toJSVal arg1
-  arg2' <- toJSVal arg2
-  arg3' <- toJSVal arg3
-  arg4' <- toJSVal arg4
-  arg5' <- toJSVal arg5
-  arg6' <- toJSVal arg6
-  arg7' <- toJSVal arg7
-  arg8' <- toJSVal arg8
-  call8_ func' arg1' arg2' arg3' arg4' arg5' arg6' arg7' arg8'
-
-call9 :: (ToJSVal a, ToJSVal b, ToJSVal c, ToJSVal d, ToJSVal e, ToJSVal f, ToJSVal g, ToJSVal h, ToJSVal i, ToJSVal j) => a -> b -> c -> d -> e -> f -> g -> h -> i -> j -> IO JSVal
-call9 func arg1 arg2 arg3 arg4 arg5 arg6 arg7 arg8 arg9 = do
-  func' <- toJSVal func
-  arg1' <- toJSVal arg1
-  arg2' <- toJSVal arg2
-  arg3' <- toJSVal arg3
-  arg4' <- toJSVal arg4
-  arg5' <- toJSVal arg5
-  arg6' <- toJSVal arg6
-  arg7' <- toJSVal arg7
-  arg8' <- toJSVal arg8
-  arg9' <- toJSVal arg9
-  call9_ func' arg1' arg2' arg3' arg4' arg5' arg6' arg7' arg8' arg9'
+  toJSVal = unsafePerformIO . toJSArray
 
 class ListBuilder r where
   build :: [JSVal] -> r
 
 instance ListBuilder [JSVal] where
-  build = reverse
+  build = id
 
-instance (ListBuilder next_r) => ListBuilder (JSVal -> next_r) where
-  build acc x = build (x : acc)
+instance (ToJSVal a, ListBuilder next_r) => ListBuilder (a -> next_r) where
+  build acc x = build ((toJSVal x) : acc)
 
 class MCompose f gresult result | f gresult -> result where
   mcomp :: f -> ([JSVal] -> gresult) -> result
@@ -208,16 +92,19 @@ instance MCompose (a -> [JSVal]) c (a -> c) where
 instance (MCompose (f -> f') gresult result) => MCompose (a -> f -> f') gresult (a -> result) where
   mcomp f g a = mcomp (f a) g
 
+callList :: JSVal -> [JSVal] -> IO JSVal
+callList f list = toJSArray list >>= callList_ f
+
+-- Gets arguments first and the function last
+-- Should be called with type argument(for now):
+-- call @(Arg1 -> Arg2 -> ... -> ArgN -> JSVal -> [JSVal])
 call :: forall builder result. (ListBuilder builder, MCompose builder (IO JSVal) result) => result
 call =
   (build [] :: builder) `mcomp` \case
     [] -> error "call requires at least one argument"
     (x : xs) -> callList x xs
 
-(~>) :: forall builder result b. (ListBuilder builder, MCompose builder (IO JSVal) result, ToJSVal b) => JSVal -> b -> result
-a ~> b = (build [] :: builder) `mcomp` ((a .> b >>=) . flip callList)
-
--- I still need to make it all work with ToJSVal
-
--- EXAMPLE:
---   print =<< call @(JSVal -> JSVal -> [JSVal]) 1 2
+-- Call member function, like
+-- window .>> "history" ~> "back"
+(~>) :: (ToJSVal a) => JSVal -> a -> IO JSVal
+a ~> b = a .> b >>= call0
--- Main.hs
+++ Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 
 module Main where
@@ -8,25 +9,24 @@ import GHC.JS.Prim (JSVal, toJSInt, toJSString)
 import JS
 import Relude
 
-log :: (ToJSVal a) => a -> IO ()
-log a = void $ (window .>> "console" .>> "log") >>= llac a
+log :: (ToJSVal arg) => arg -> IO ()
+log a = void $ (window .>> "console" .>> "log") >>= call @(JSVal -> JSVal -> [JSVal]) (toJSVal a)
 
-arc :: JSVal -> Int -> Int -> Int -> Int -> Double -> IO JSVal
-arc ctx cx cy r start end = (ctx .> "arc") >>= \f -> call5 f cx cy r start end
+arc :: JSVal -> Int -> Int -> Int -> Double -> Double -> IO JSVal
+arc ctx cx cy r start end = ctx .> "arc" >>= call @(Int -> Int -> Int -> Double -> Double -> JSVal -> [JSVal]) cx cy r start end
 
 plot :: String -> IO ()
 plot id = do
   log "Hello log from JS"
-  window .>> "alert" >>= llac "Hello alert from JS"
-  window .>> "console" .>> "log" >>= llac id
-  canvas <- window .>> "document" .>> "getElementById" >>= llac id
-  ctx <- canvas .> "getContext" >>= llac "2d"
+  window .>> "alert" >>= call @(String -> JSVal -> [JSVal]) "Hello alert from JS"
+  canvas <- window .>> "document" .>> "getElementById" >>= call @(String -> JSVal -> [JSVal]) id
 
   let width = 700 :: Int
       height = 300 :: Int
   set canvas "width" width
   set canvas "height" height
 
+  ctx <- canvas .> "getContext" >>= call @(String -> JSVal -> [JSVal]) "2d"
   ctx ~> "beginPath"
   arc ctx 150 150 80 0 (2 * 3.14159)
   set ctx "fillStyle" "#10b981"
@@ -34,12 +34,9 @@ plot id = do
   set ctx "strokeStyle" "#065f46"
   set ctx "lineWidth" (4 :: Int)
   ctx ~> "stroke"
-  -- (~>) @(JSVal -> String -> JSVal -> IO JSVal) ctx "closePath"
-  -- ctx ~> "closePath"
-  -- void $ (ctx ~> "closePath")
-  void $ (~>) @([JSVal]) ctx "closePath"
-  alert <- window .>> "alert"
-  void $ call @(JSVal -> JSVal -> [JSVal]) alert (toJSString "polyvariadic")
+  ctx ~> "closePath"
+  window .>> "alert" >>= call @(String -> JSVal -> [JSVal]) "polyvariadic"
+  pure ()
 
 main :: IO ()
 main = plot "canvas"
